60c9d9bcc5956594658ad6838a0761ac821c63f2
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2020-05-16 13:03:59 -0400

    Added the LaunchAPI class and then added async function to retrieve the various launches and used a reducer. Not that I'm sure what a reducer does yet....

---
+++ start/server/src/datasources/launch.js
@@ -0,0 +1,16 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL ='https://api.spacexdata.com/v2/';
+  }
+  async getAllLaunches() {
+    const response = await this.get('launches');
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch))
+      : [];
+  }
+}
+
+module.exports = LaunchAPI;
